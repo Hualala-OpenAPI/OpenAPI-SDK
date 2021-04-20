cccf56e35cf7014e20efcffc233fcb97002b105c
Sze Lu <silu@hualala.com>
2021-04-20 14:42:54 +0800

    修改一处常亮

--- demo/src/main/java/com/hualala/openapi/demo/Demo.java
+++ demo/src/main/java/com/hualala/openapi/demo/Demo.java
@@ -2,6 +2,7 @@ package com.hualala.openapi.demo;
 
 import com.alibaba.fastjson.JSONObject;
 import com.hualala.openapi.sdk.consts.SDKConst;
+import com.hualala.openapi.sdk.consts.SDKEnv;
 import com.hualala.openapi.sdk.helper.APIHelper;
 import com.hualala.openapi.sdk.intf.OpenAPIEngine;
 import lombok.extern.slf4j.Slf4j;
@@ -10,7 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 public class Demo {
 
     public static void main(String[] arg) {
-        System.setProperty(SDKConst.ENV_KEY, "DEV");
+        System.setProperty(SDKConst.ENV_KEY, SDKEnv.DEV.name());
         System.setProperty(SDKConst.APP_KEY_KEY, "1010");
         System.setProperty(SDKConst.APP_SECRET_KEY, "bRFVbWHQ");
 
