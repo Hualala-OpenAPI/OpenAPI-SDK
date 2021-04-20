f09cb145d27e87e3f81904d6e2128b991bb5b8a3
SiLu <silu@hualala.com>
2021-04-20 18:47:24 +0800

    方便区分环境测试

--- demo/src/main/java/com/hualala/openapi/demo/Demo.java
+++ demo/src/main/java/com/hualala/openapi/demo/Demo.java
@@ -11,11 +11,25 @@ import lombok.extern.slf4j.Slf4j;
 public class Demo {
 
     public static void main(String[] arg) {
+        testDev();
+//        testOnline();
+    }
+
+    private static void testDev() {
         System.setProperty(SDKConst.ENV_KEY, SDKEnv.DEV.name());
         System.setProperty(SDKConst.APP_KEY_KEY, "1010");
         System.setProperty(SDKConst.APP_SECRET_KEY, "bRFVbWHQ");
 
         OpenAPIEngine engine = new APIHelper();
         System.out.println(JSONObject.toJSONString(engine.getDocBaseInfo(1155L, 76068673L)));
+    }
+
+    private static void testOnline() {
+        OpenAPIEngine engine = new APIHelper();
+        System.setProperty(SDKConst.ENV_KEY, SDKEnv.ONLINE.name());
+
+        System.setProperty(SDKConst.APP_KEY_KEY, "");
+        System.setProperty(SDKConst.APP_SECRET_KEY, "");
+        System.out.println(JSONObject.toJSONString(engine.getDocBaseInfo(0L, 0L)));
     }
 }
