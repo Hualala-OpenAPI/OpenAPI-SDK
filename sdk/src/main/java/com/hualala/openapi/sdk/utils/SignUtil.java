77ca52ad24acfb034b316db060cabe3e0f1f4573
SzeLu <82429216+Hualala-OpenAPI@users.noreply.github.com>
2022-09-21 10:01:13 +0800

    开放 version 3 签名规则, 并设置为默认签名方式

--- sdk/src/main/java/com/hualala/openapi/sdk/utils/SignUtil.java
+++ sdk/src/main/java/com/hualala/openapi/sdk/utils/SignUtil.java
@@ -13,6 +13,7 @@ import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -177,5 +178,21 @@ public class SignUtil {
         }
 
         return null;
+    }
+
+    public static String get32MD5(String string) {
+        byte[] hash;
+        try {
+            hash = MessageDigest.getInstance("MD5").digest(string.getBytes(StandardCharsets.UTF_8));
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Huh, MD5 should be supported?", e);
+        }
+
+        StringBuilder hex = new StringBuilder(hash.length * 2);
+        for (byte b : hash) {
+            if ((b & 0xFF) < 0x10) hex.append("0");
+            hex.append(Integer.toHexString(b & 0xFF));
+        }
+        return hex.toString();
     }
 }
